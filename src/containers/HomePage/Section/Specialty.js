d560822c8c7704b9bedf436ac72cbf72c3587340
ncp204 <19130172@st.hcmuaf.edu.vn>
2023-07-26 16:59:41 +0700

    Customize Tùy Chỉnh React-Slider-Carousel

--- src/containers/HomePage/Section/Specialty.js
+++ src/containers/HomePage/Section/Specialty.js
@@ -8,6 +8,9 @@ import 'slick-carousel/slick/slick-theme.css'
 
 import './Specialty.scss';
 
+import img from '../../../assets/images/specialty/112457-co-xuong-khop.jpg'
+import { Button } from "reactstrap";
+
 class Specialty extends Component {
 
     render() {
@@ -17,32 +20,42 @@ class Specialty extends Component {
             dots: false,
             infinite: true,
             speed: 500,
-            slidesToShow: 1,
-            slidesToScroll: 1
+            slidesToShow: 4,
+            slidesToScroll: 2,
         }
 
         return (
             <>
                 <div className="section-specialty">
                     <div className="specialty-content">
+                        <div className="section-header">
+                            <span className="section-title" >Chuyên khoa phổ biến</span>
+                            <Button className="btn-title" color="primary">Xem thêm</Button>
+                        </div>
                         <Slider {...settings}>
                             <div className="img-customize">
-                                <h3>1</h3>
+                                <img src={img} />
+                                <h3>Cơ xương khớp</h3>
                             </div>
                             <div className="img-customize">
-                                <h3>2</h3>
+                                <img src={img} />
+                                <h3>Thần kinh</h3>
                             </div>
                             <div className="img-customize">
-                                <h3>3</h3>
+                                <img src={img} />
+                                <h3>Tiêu hoá</h3>
                             </div>
                             <div className="img-customize">
-                                <h3>4</h3>
+                                <img src={img} />
+                                <h3>Tim mạch</h3>
                             </div>
                             <div className="img-customize">
-                                <h3>5</h3>
+                                <img src={img} />
+                                <h3>Tai mũi họng</h3>
                             </div>
                             <div className="img-customize">
-                                <h3>6</h3>
+                                <img src={img} />
+                                <h3>Cột sống</h3>
                             </div>
                         </Slider>
                     </div>
